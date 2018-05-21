cd37358505be0c21a6a041e13e3bc8d6a98c396f
Rob Young <rob@webteacher.com>
2018-05-20 22:19:34 -0700

    still can't get operators to work.

--- blank/src/app/app.component.ts
+++ blank/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable, fromEvent } from 'rxjs';
-
-// import { Observable, Subject, ReplaySubject, fromEvent, of, range } from 'rxjs';
-// import { map, filter, switchMap, debounce, debounceTime } from 'rxjs/operators';
-import { map, debounce, debounceTime } from 'rxjs/operators';
-
+import { debounceTime } from 'rxjs/operators';
+// import 'rxjs/add/operator/debounceTime';
 
 
 @Component({
@@ -23,3 +20,38 @@ export class AppComponent {
 }
 
 
+/*
+// typing "hello world"
+var input = Rx.Observable.fromEvent(document.querySelector('input'), 'input');
+
+// Filter out target values less than 3 characters long
+input.filter(event => event.target.value.length > 2)
+  .map(event => event.target.value)
+  .subscribe(value => console.log(value)); // "hel"
+
+// Delay the events
+input.delay(200)
+  .map(event => event.target.value)
+  .subscribe(value => console.log(value)); // "h" -200ms-> "e" -200ms-> "l" ...
+
+// Only let through an event every 200 ms
+input.throttleTime(200)
+  .map(event => event.target.value)
+  .subscribe(value => console.log(value)); // "h" -200ms-> "w"
+
+// Let through latest event after 200 ms
+input.debounceTime(200)
+  .map(event => event.target.value)
+  .subscribe(value => console.log(value)); // "o" -200ms-> "d"
+
+// Stop the stream of events after 3 events
+input.take(3)
+  .map(event => event.target.value)
+  .subscribe(value => console.log(value)); // "hel"
+
+// Passes through events until other observable triggers an event
+var stopStream = Rx.Observable.fromEvent(document.querySelector('button'), 'click');
+input.takeUntil(stopStream)
+  .map(event => event.target.value)
+  .subscribe(value => console.log(value)); // "hello" (click)
+  */
